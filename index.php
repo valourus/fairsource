b0acf226e1357e5e3c679a08b72154380ccaee07
302329760@student.rocmondriaan.nl <Anker2345>
2018-02-20 08:37:18 +0100

    animaties verbeterd

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
  * User: koenvanderels
  * Date: 19/02/2018
  * Time: 10:20
- */?>
+ */
+?>
 <html>
     <head>
+        <!--DIT IS ALLEEN OM TE TESTEN -->
+        <script src="js/animatie.js"></script>
+
         <link rel="stylesheet" type="text/css" href="css/index.css">
     </head>
     <body>
-<!--    TEMPLATE TO IMPORT A NEW PART -->
+    <!--    TEMPLATE TO IMPORT A NEW PART -->
         <?php //include "html/header.php"?>
+        <?php require_once "html/expertise.php"?>
     </body>
 </html>
