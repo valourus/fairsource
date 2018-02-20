32fb18252171777df811d2cd4278f93e439e3dd8
koenvanderels <koenvanderels@LAPTOP-KP7FV2G0>
2018-02-20 16:04:40 +0100

    made the footer

---
+++ html/footer.php
@@ -0,0 +1,12 @@
+<section class="footer">
+    <div class="footer__text">
+        <b>copyright</b>
+        <h4 class="footer__h4">FAIRSOURCE</h4>
+        <h4 class="footer__h4 footer__h4--black">2016</h4>
+    </div>
+    <div class="footer__images">
+        <figure><img class="footer__icon" src="img/facebook.png"/></figure>
+        <figure><img class="footer__icon" src="img/linked in.png"/></figure>
+        <figure><img class="footer__icon" src="img/twitter.png"/></figure>
+    </div>
+</section>
