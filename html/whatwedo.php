0f756b0da4630d04867d8d9c6f7268a99cd5ad1b
koenvanderels <koenvanderels@LAPTOP-KP7FV2G0>
2018-02-20 13:45:26 +0100

    added margin -10px

--- html/whatwedo.php
+++ html/whatwedo.php
@@ -8,4 +8,5 @@
 
 <section class="whatwedo">
     <header class="whatwedo__header">WHAT WE DO</header>
+    <p class="whatwedo__p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non sem vitae quam porttitor efficitur tristique vitae dui. Curabitur ultrices consequat tincidunt. Duis ut magna pretium, rutrum felis non, hendrerit justo. Nullam ut elit metus. Phasellus et nunc justo. Cras elementum bibendum mauris sed cursus. Praesent ut aliquam ante, sit amet tincidunt felis.</p>
 </section>
