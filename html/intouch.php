3fca2e1e674a73b716edcab253da4b113d2703d1
302329760@student.rocmondriaan.nl <Anker2345>
2018-02-21 12:56:53 +0100

    intouch verberterd

--- html/intouch.php
+++ html/intouch.php
@@ -16,10 +16,12 @@
             <div class="form__head">
                 SEND US A MESSAGE
             </div>
-            <input class="form__name form__input" type="text" value="Type your name here"/ >
-            <input id="email" class="form__email form__input" type="email" value=" Type your email address here"/>
-            <textarea class="form__textarea form__input" rows="10" cols="50">What is it you want to have contact about?
+            <input class="form__name form__input" type="text" placeholder="Type your name here"/ >
+            <input id="email" class="form__email form__input" type="email" placeholder="Type your email address here"/>
+
+            <textarea placeholder="What is it you want to have contact about?" class="form__input form__textarea" rows="10" cols="50">What is it you want to have contact about?
             </textarea>
+
             <input class="form__submit" type="submit" value="send">
         </form>
 
