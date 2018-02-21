243761a239ca19f56743ec31fb5c369e66b75be1
koenvanderels <koenvanderels@LAPTOP-KP7FV2G0>
2018-02-21 16:11:32 +0100

    fixed js for real xD

--- js/animatie.js
+++ js/animatie.js
@@ -23,39 +23,37 @@ window.addEventListener('scroll', function() {
 });
 
 //Module *****************************************************
-//
-// var BlocksSlideIn = (function () {
-//     var Public = {};
-//     var self = {};
-//
-//     self.blocks = document.getElementsByClassName('expertise__block');
-//     console.log(self.blocks);
-//
-//     window.addEventListener('scroll', function () {
-//         Public.onScroll();
-//         console.log("help");
-//     });
-//
-//     Public.onScroll = function(){
-//         count = 1;
-//         for(var block of self.blocks){
-//             console.log(elementInViewport(block));
-//             if(elementInViewport(block)){
-//
-//                 block.style.opacity = '0.7';
-//                 block.style.left = '0px';
-//             }else{
-//
-//                 if(count % 2 === 0){
-//                     block.style.left = '125px';
-//                 }else{
-//                     block.style.left = '-125px';
-//                 }
-//                 block.style.opacity = '0';
-//             }
-//             count++;
-//         }
-//     };
-//
-//     return Public;
-// })();
+
+var BlocksSlideIn = (function () {
+    var Public = {};
+    var self = {};
+
+    self.blocks = document.getElementsByClassName('expertise__block');
+    console.log(self.blocks);
+
+    window.addEventListener('scroll', function () {
+        Public.onScroll();
+        console.log("help");
+    });
+
+    Public.onScroll = function(){
+        for(var i = 0;i < blocks.length;i++) {
+            console.log(elementInViewport(blocks[i]));
+            if(elementInViewport(blocks[i])){
+
+                blocks[i].style.opacity = '0.7';
+                blocks[i].style.left = '0px';
+            }else{
+
+                if(i % 2 === 0){
+                    blocks[i].style.left = '125px';
+                }else{
+                    blocks[i].style.left = '-125px';
+                }
+                blocks[i].style.opacity = '0';
+            }
+        }
+    };
+
+    return Public;
+})();
