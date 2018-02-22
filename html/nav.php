9f4e8aca8271cc0b1684d6c6a707038ee8adc36a
SHoogenboom <shoogenboom@competa.com>
2018-02-22 09:46:43 +0100

    bla bla ja

--- html/nav.php
+++ html/nav.php
@@ -2,7 +2,7 @@
 <header class="page__header">
     <nav class="page__nav">
         <a href="#" onclick="return false" onmousedown="ScrollModule.autoScroll('header')"> <img class="header__logo" src="img/logo.png" ></a>
-        <a href="#" onclick="return false;" onmousedown="ScrollModule.autoScroll('contact');" class="nav__text--green">CONTACT</a>
+        <a href="#" onclick="return false;" onmousedown="ScrollModule.autoScroll('contact');" class="nav__text nav__text--green">CONTACT</a>
         <a href="#" onclick="return false;" onmousedown="ScrollModule.autoScroll('links');" class="nav__text">LINKS/RESOURCES</a>
         <a href="#" onclick="return false;" onmousedown="ScrollModule.autoScroll('services');" class="nav__text">SERVICES</a>
         <a href="#" onclick="return false;" onmousedown="ScrollModule.autoScroll('expertise');" class="nav__text">EXPERTISE</a>
